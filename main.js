019ed46d04e70225e943e1db9cccb7cdaebd81d9
UbaidRussell <UbaidRussell@users.noreply.github.com>
2023-05-01 15:25:34 -0400

    VERSION 1.2📦

--- main.js
+++ main.js
@@ -4,9 +4,7 @@ const randomHoroscope = horoscope[Math.floor(Math.random() * horoscope.length)]
 const loveAdvice = ['You will find love in the most unexpected place', 'You will find love in the most expected place', 'You will find love at the most unexpected time', 'You will find love in the most expected time']
 const randomLoveAdvice = loveAdvice[Math.floor(Math.random() * loveAdvice.length)]
 
-const moneyAdvice =  ['Today, hold on to your money', 'Today, Spend your money', 'Today, Invest your money', 'Today, share your money with charity', 'Today, use your money to buy a gift for someone special', 'Today, use your money to buy a gift for yourself']
+const moneyAdvice =  ['hold on to your money', 'Spend your money', 'Invest your money', 'share your money with charity', 'use your money to buy a gift for someone special', 'use your money to buy a gift for yourself']
 const randomMoneyAdvice = moneyAdvice[Math.floor(Math.random() * moneyAdvice.length)]
 
-console.log(randomHoroscope)
-console.log(randomLoveAdvice)   
-console.log(randomMoneyAdvice)
+console.log(`Hello visitor, your horoscope for today is ${randomHoroscope}. Your love advice for today is that ${randomLoveAdvice}. Your money advice for today is ${randomMoneyAdvice}.`)
