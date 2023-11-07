2ff3f910bc1527f91191ce155637756863d33273
UbaidRussell <UbaidRussell@users.noreply.github.com>
2023-11-06 23:48:33 -0500

    Refactored the generateMessage function 🔧

--- javascript/link.js
+++ javascript/link.js
@@ -1,8 +1,9 @@
 import { horoscopegenerator, loveAdviceGenerator, moneyAdviceGenerator } from './main.js';
-let paragraph = document.getElementById('message')[0];
+let body = document.querySelector('body');
+let paragraph = document.querySelector('p')[0];
 
-function generateMessage(){
-    return `Hello visitor, your horoscope for today is ${horoscopegenerator}. Your love advice for today is that ${loveAdviceGenerator}. Your money advice for today is ${moneyAdviceGenerator}.`
+
+
+body.onload = function(){
+    paragraph.innerHTML = `Hello visitor, your horoscope for today is ${horoscopegenerator}. Your love advice for today is that ${loveAdviceGenerator}. Your money advice for today is ${moneyAdviceGenerator}.`;
 }
-
-paragraph.innerHTML = generateMessage();
