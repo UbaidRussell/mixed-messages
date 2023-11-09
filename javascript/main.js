f516faa10c49eded854c2d5360ccf1e7c2d48e54
UbaidRussell <UbaidRussell@users.noreply.github.com>
2023-11-09 13:36:14 -0500

    got random message to generate 🔧

--- javascript/main.js
+++ javascript/main.js
@@ -1,16 +1,28 @@
+let body = document.querySelector('body');
+let paragraph = document.querySelector('p');
+
 //Calculation file
-function horoscopegenerator(){
+function horoscopegenerator(randomHoroscope){
     const horoscope = ['Aries', 'Tarus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagitarius', 'Capricorn', 'Aquarius', 'Pisces']
-    return randomHoroscope = horoscope[Math.floor(Math.random() * horoscope.length)]
+    randomHoroscope = horoscope[Math.floor(Math.random() * horoscope.length)]
+    return randomHoroscope;
 }
-function loveAdviceGenerator(){
+function loveAdviceGenerator(randomLoveAdvice){
     const loveAdvice = ['You will find love in the most unexpected place', 'You will find love in the most expected place', 'You will find love at the most unexpected time', 'You will find love in the most expected time']
-    return randomLoveAdvice = loveAdvice[Math.floor(Math.random() * loveAdvice.length)]
+    randomLoveAdvice = loveAdvice[Math.floor(Math.random() * loveAdvice.length)]
+    return randomLoveAdvice;
 }
 
-function moneyAdviceGenerator(){
+function moneyAdviceGenerator(randomMoneyAdvice){
     const moneyAdvice =  ['hold on to your money', 'Spend your money', 'Invest your money', 'share your money with charity', 'use your money to buy a gift for someone special', 'use your money to buy a gift for yourself']
-    return randomMoneyAdvice = moneyAdvice[Math.floor(Math.random() * moneyAdvice.length)]
+    randomMoneyAdvice = moneyAdvice[Math.floor(Math.random() * moneyAdvice.length)]
+    return randomMoneyAdvice;
 }
 
-export { horoscopegenerator, loveAdviceGenerator, moneyAdviceGenerator};    
+
+function theChange(){
+    paragraph.innerHTML = `Hello visitor, your horoscope for today is ${horoscopegenerator()}. Your love advice for today is that ${loveAdviceGenerator()}. Your money advice for today is ${moneyAdviceGenerator()}.`;
+}
+
+
+theChange();
